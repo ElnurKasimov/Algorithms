174423f50d0a028f039659edb0c8bfff5b52f466
Elnur <elnur.kasimov@grand-logistic.com>
2023-02-28 17:20:09 +0200

    altered RandomWord

--- src/RandomWord.java
+++ src/RandomWord.java
@@ -1,19 +1,30 @@
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
     public static void main(String[] args) {
 //        String champion = "";
 //        int i=1;
-//        while(StdIn.hasNextLine()) {
-//            String input = StdIn.readString();
-//            if(Math.random() < 1/i) {
+//        String input = "";
+//        do {
+//            input = StdIn.readString();
+//            double poss = 1./i;
+//            boolean check = StdRandom.bernoulli(poss);
+//            if(check) {
 //                champion = input;
 //            };
 //            i++;
-//        }
-            for (String line : args) {
-                System.out.println(line);
-            }
-
+//        } while(!StdIn.isEmpty());
+//        System.out.println(champion);
+//    }
+        String champion = "";
+        int i=1;
+        for (String word : args) {
+            if(StdRandom.bernoulli(1./i)) {
+                champion = word;
+            };
+            i++;
+        }
+        System.out.println(champion);
     }
 }
