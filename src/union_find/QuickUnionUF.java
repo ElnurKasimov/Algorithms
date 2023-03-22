dd9fa62dde90230f3a16fa1ade273da851ef144a
ElnurKasimov <ekasimov2012@gmail.com>
2023-03-22 22:23:15 +0200

    created QuickUnion another implementation of  UF

---
+++ src/union_find/QuickUnionUF.java
@@ -0,0 +1,13 @@
+package union_find;
+
+public class QuickUnionUF  implements UF{
+    @Override
+    public boolean connected(int p, int q) {
+        return false;
+    }
+
+    @Override
+    public void union(int p, int q) {
+
+    }
+}
